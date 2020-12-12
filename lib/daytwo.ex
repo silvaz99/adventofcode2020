fa283febc50ed537fd4f1eb42f6c40967e1076a6
silvaz99 <lucasilvaz99@hotmail.com>
2020-12-11 21:14:57 -0300

    Day two pt2, i know i'm late

--- lib/daytwo.ex
+++ lib/daytwo.ex
@@ -9,11 +9,58 @@ defmodule Daytwo do
 
   end
 
+  def main_pt2 do
+
+    rows = get_input()
+
+    Enum.map(rows, fn x -> count_valid_pt2(x) end)
+    |> Enum.sum()
+
+  end
+
+  def count_valid_pt2(row) do
+    [ numbers , letter_with_colon , password ] = String.split(row, " ", trim: true)
+
+    [ min, max ] = get_min_max(numbers)
+
+    letter = String.first(letter_with_colon)
+
+
+    if check_one_logic_part(password, min, max, letter) and check_two_logic_part(password, min, max, letter)  do
+      1
+    else
+      0
+    end
+  end
+
+  def check_one_logic_part(password, min, max, letter) do
+    if String.at(password, min - 1) == letter or String.at(password, max - 1) == letter do
+      true
+    else
+      false
+    end
+  end
+
+  def check_two_logic_part(password, min, max, letter) do
+    if not (String.at(password, min - 1) == letter) or not (String.at(password, max - 1) == letter) do
+      true
+    else
+      false
+    end
+
+  end
+
+  def get_min_max(numbers) do
+
+    String.split(numbers, "-", trim: true)
+    |> Enum.map(&String.to_integer/1)
+
+  end
+
   def count_valid(row) do
     [ numbers , letter | password ] = String.split(row, " ", trim: true)
 
-    [ min , max ] = String.split(numbers, "-", trim: true)
-    |> Enum.map(&String.to_integer/1)
+    [ min , max ] = get_min_max(numbers)
 
     chars_counted = count_letter(String.first(letter), password)
 
