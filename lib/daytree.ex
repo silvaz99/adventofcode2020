2c33208513b250f4390f9ffb4bd9d35d2694d349
silvaz99 <lucasilvaz99@hotmail.com>
2020-12-11 23:14:19 -0300

    Day 3 part 2 complete

--- lib/daytree.ex
+++ lib/daytree.ex
@@ -4,16 +4,32 @@ defmodule Daytree do
     rows = process_input()
     [_head | tail] = rows
 
-    tail
-    |> Enum.map_reduce({0, 3}, fn x, {tree, col} -> {
-      check_tree(x, {tree, col}),
-      {tree, check_col(col)} }
-    end)
-    |> sum_trees()
+    list = [{0, 1}, {0, 3}, {0, 5}, {0, 7}]
+
+    partial_result = Enum.map(list, fn {x, y} -> idontknow({x, y}, tail) end)
+    |> Enum.reduce(fn x, acc -> x * acc end)
+
+    new_rows = Enum.drop_every(tail, 2)
+
+    every_two = Enum.map([{0,1}], fn {x, y} -> idontknow({x, y}, new_rows) end)
+    |> Enum.sum()
+
+    partial_result * every_two
 
   end
 
+
+  def idontknow({initial_x, initial_y}, tail) do
+    tail
+    |> Enum.map_reduce({initial_x, initial_y}, fn x, {tree, col} -> {
+      check_tree(x, {tree, col}),
+      {tree, check_col(col, initial_y)} }
+    end)
+    |> sum_trees()
+  end
+
   def sum_trees({trees, {_, _}}) do
+    IO.puts(Enum.sum(trees))
     Enum.sum(trees)
   end
 
@@ -25,11 +41,11 @@ defmodule Daytree do
     end
   end
 
-  def check_col(col) do
-    if col + 3 > 30 do
-      rem(col + 3, 30) - 1
+  def check_col(col, step) do
+    if col + step > 30 do
+      rem(col + step, 30) - 1
     else
-      col + 3
+      col + step
     end
   end
 
